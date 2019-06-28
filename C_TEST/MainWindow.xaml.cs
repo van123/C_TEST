b23079e9b22868a7861c4e0aa67b5eaa1bb1e6f1
van123 <daashton1@googlemail.com>
2019-06-29 09:10:12 +1000

    dev2

--- C_TEST/MainWindow.xaml.cs
+++ C_TEST/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace C_TEST
 {
     /// <summary>
@@ -23,6 +24,40 @@ namespace C_TEST
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            double num1;
+            double num2;
+            double num3;
+            bool canConvert1;
+            bool canConvert2;
+
+            canConvert1 = double.TryParse(tb1.Text, out num1);
+            canConvert2 = double.TryParse(tb2.Text, out num2);
+
+            if (canConvert1 && canConvert2)
+            {
+                num3 = num1 + num2;
+                tb3.Text = Convert.ToString(num3);
+            }
+            else
+            {
+                string message = "Numbers must be entered, close the application?";
+                string title = "Error";
+
+                MessageBoxResult result = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Information, MessageBoxResult.No);
+
+                if (result == MessageBoxResult.Yes)
+                {  
+                    this.Close();
+                }
+                else
+                {
+                    
+                }
+            }
         }
     }
 }
